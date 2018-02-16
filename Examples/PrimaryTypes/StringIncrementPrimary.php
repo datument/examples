7d22f359bdb973e8ef3840de8b04f7490fa4f6d0
Fenz <uukoo@163.com>
2018-02-16 21:53:22 +0800

    Add model with string-increment primary for primary types example

---
+++ Examples/PrimaryTypes/StringIncrementPrimary.php
@@ -0,0 +1,12 @@
+<?php
+
+declare( strict_types= 1 );
+
+namespace Datument\Examples\PrimaryTypes;
+
+////////////////////////////////////////////////////////////////
+
+class StringIncrementPrimary extends \Datument\AModel
+{
+	#
+}
---
+++ Examples/PrimaryTypes/StringIncrementPrimary/Set.php
@@ -0,0 +1,12 @@
+<?php
+
+declare( strict_types= 1 );
+
+namespace Datument\Examples\PrimaryTypes\StringIncrementPrimary;
+
+////////////////////////////////////////////////////////////////
+
+class Set extends \Datument\Set
+{
+	#
+}
