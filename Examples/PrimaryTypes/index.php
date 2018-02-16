5d7b21acbaa87da6274c8f68ee8f21715c62f769
Fenz <uukoo@163.com>
2018-02-16 21:45:39 +0800

    comment of primary key types

--- Examples/PrimaryTypes/index.php
+++ Examples/PrimaryTypes/index.php
@@ -7,3 +7,14 @@ namespace Datument\Examples\PrimaryTypes;
 use Datument\Datument as D;
 
 ////////////////////////////////////////////////////////////////
+
+/**
+ * Datument allows several types of primary key.
+ *   simple
+ *     increment (default)
+ *     integer
+ *     string
+ *
+ *   complex
+ *     ...
+ */
