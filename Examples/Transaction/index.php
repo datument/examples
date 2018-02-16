e3bc906a7d5742b5cc1be9c103529ec6c8901655
Fenz <uukoo@163.com>
2018-02-16 21:59:32 +0800

    manually transaction example

--- Examples/Transaction/index.php
+++ Examples/Transaction/index.php
@@ -24,3 +24,19 @@ T::do(
 
 // The transaction will be rolled back if a \Throwable been throwed,
 // but will be commited otherwise.
+
+
+
+
+/**
+ * Manually using.
+ */
+
+T::start();
+
+// transaction code...
+
+if( $needCommit )
+	T::commit();
+else
+	T::rollback();
