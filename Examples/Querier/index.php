1355762b857ff611b63b0fd41d1e42508d9fdd3c
Fenz <uukoo@163.com>
2018-02-16 21:21:07 +0800

    querier getting example

--- Examples/Querier/index.php
+++ Examples/Querier/index.php
@@ -7,3 +7,20 @@ namespace Datument\Examples\Querier;
 use Datument\Querier as Q;
 
 ////////////////////////////////////////////////////////////////
+
+/***
+ * Get a querier.
+ */
+
+// Create with constructor
+$querier= new Q;
+
+// Create with static magic methods from Querier class.
+$querier= Q::from( 'User' );
+
+// Create with static magic methods from Model class.
+$querier= User::where( 'age', '>', 5 );
+
+// From entity set.
+$users= User::getBy( 'age', 8 );
+$querier= $users->querier;
