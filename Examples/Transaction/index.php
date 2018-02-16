c3207da990a8fbaace4dcf1f2488d3cf91d41a5e
Fenz <uukoo@163.com>
2018-02-16 21:57:48 +0800

    basic transaction example

--- Examples/Transaction/index.php
+++ Examples/Transaction/index.php
@@ -7,3 +7,20 @@ namespace Datument\Examples\Transaction;
 use Datument\Transaction as T;
 
 ////////////////////////////////////////////////////////////////
+
+/**
+ * With callable (basic and recommended).
+ */
+T::do(
+	function(){
+		$foo= Foo::lock( 'R' )->find( 1 );
+		$bar= $foo->Bar->lock( 'W' )->get();
+		$baz= $foo->Baz->lock( 'W' )->get();
+
+		$bar->increate( 'bar' );
+		$baz->increate( 'baz' );
+	}
+);
+
+// The transaction will be rolled back if a \Throwable been throwed,
+// but will be commited otherwise.
