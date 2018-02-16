abb0b9ffac91fe6599791511a36c150aad818f19
Fenz <uukoo@163.com>
2018-02-16 22:15:54 +0800

    comment of static model

--- Examples/StaticModel/index.php
+++ Examples/StaticModel/index.php
@@ -5,3 +5,10 @@ declare( strict_types= 1 );
 namespace Datument\Examples\StaticModel;
 
 ////////////////////////////////////////////////////////////////
+
+/**
+ * To static model, data are just stored within code.
+ * It's fit for storing meta data with a small amount and few changing,
+ * provide fast accessibility and same behaviors with normal models.
+ * You should update data via updating code, runtime creating, updating, deleting are denied.
+ */
